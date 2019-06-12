7f34c44898c497d7bea16e331360b3756038b4bd
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-06-11 20:39:28 -0400

    set up variables / selceted dice elements

--- app.js
+++ app.js
@@ -8,3 +8,43 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+
+let scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+
+// Get integer, Get random number 0 - 6 , get number 1-  6
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+// getter ^ 
+
+let x = document.querySelector('#score-0').textContent;
+// console.log(x);
+//setter ^
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function (){
+    
+    // 1. Randoom Number 
+    let dice = Math.floor(Math.random() * 6) + 1; 
+    console.log(dice);
+    //2 Display the Result 
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3 Update the round score IF the rolled number was NOt a 1
+
+
+});
